8262398bad270754dcc9eedc59479b1942ba8a75
Patil <aniket.a.patil@capgemini.com>
2024-02-06 15:07:00 +0530

    first stage completed

---
+++ src/CNNClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,15 @@
+from src.CNNClassifier.components.stage_01_data_ingestion import DataIngestion
+from src.CNNClassifier.config.configuration import ConfigurationManager
+from src.CNNClassifier import logger
+
+
+
+logger.info(f"data ingesiton stage started")
+
+config = ConfigurationManager()
+data_ingestion_config = config.get_data_ingestion_config()
+data_ingestion = DataIngestion(config=data_ingestion_config)
+data_ingestion.download_file()
+data_ingestion.unzip_and_clean()
+
+logger.info(f"data ingesiton stage completed")
